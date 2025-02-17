092078b90214c227338808cc5e6ed8c0915f3004
Sean Kannanaikal <stk5106@rit.edu>
2025-02-17 09:11:48 -0500

    added functionality to normalize data

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from FLEM import FLEM_FRAMEWORK
 from flask import Flask, render_template, request, session
-from stats import genPieChart, genBarChart, genHistogram
+from stats import genPieChart, genBarChart, genHistogram, normalizeData
 
 app = Flask(__name__)
 app.secret_key = 'blah'
@@ -32,11 +32,20 @@ def loadingPage():
 def displayResults():  
     #Maybe run the FLEM framework for multiple iterations and then take the average highest ranked stuff
     rankedMaliciousFunctions, sortedAttributions, attributionsNP = FLEM_FRAMEWORK(session['filepath'], session['model'], session['algorithm'])
-    genPieChart(attributionsNP[0])
     sortedAttributionScores = [attributionsNP[0][sortedAttributions[i]] for i in range(len(sortedAttributions))]
-    genBarChart(rankedMaliciousFunctions[:10], sortedAttributionScores[:10])
     genHistogram(sortedAttributionScores)
-    return render_template('results.html', rankedMaliciousFunctions=rankedMaliciousFunctions, sortedAttributions=sortedAttributions, attributionsNP=attributionsNP)
+    normalizedAttributions = normalizeAttributions(attributionsNP, sortedAttributions)
+    return render_template('results.html', rankedMaliciousFunctions=rankedMaliciousFunctions, sortedAttributions=sortedAttributions, attributionsNP=normalizeAttributions)
+
+def normalizeAttributions(attributions, sortedAttributions):
+    positiveAttributionScores = [attributions[0][sortedAttributions[i]] for i in range(len(sortedAttributions)) if attributions[0][sortedAttributions[i]] > 0]
+    negativeAttributionScores = [attributions[0][sortedAttributions[i]] for i in range(len(sortedAttributions)) if attributions[0][sortedAttributions[i]] <= 0]
+
+    positiveNormalized = normalizeData(positiveAttributionScores)
+    negativeNormalized = normalizeData(negativeAttributionScores)
+
+    return positiveNormalized + negativeAttributionScores
+
 
 if __name__ == '__main__':
     app.run()
