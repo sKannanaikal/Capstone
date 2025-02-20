ccbf7ba4065dad429870741376aca415853dd9fb
Sean Kannanaikal <stk5106@rit.edu>
2025-02-20 14:22:47 -0500

    working on client frontend

---
+++ client/src/App.tsx
@@ -0,0 +1,147 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+"use client"
+import {
+  useState
+} from "react"
+import {
+  toast
+} from "sonner"
+import {
+  useForm
+} from "react-hook-form"
+import {
+  zodResolver
+} from "@hookform/resolvers/zod"
+import * as z from "zod"
+import {
+  cn
+} from "@/lib/utils"
+import {
+  Button
+} from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue
+} from "@/components/ui/select"
+
+import { Input } from './components/ui/input'
+
+const formSchema = z.object({
+  fileInput: z.string(),
+  model: z.string(),
+  algorithm: z.string()
+});
+
+export default function MyForm() {
+
+  const [files, setFiles] = useState(null);
+
+  const dropZoneConfig = {
+    maxFiles: 5,
+    maxSize: 1024 * 1024 * 4,
+    multiple: true,
+  };
+  const form = useForm < z.infer < typeof formSchema >> ({
+    resolver: zodResolver(formSchema),
+
+  })
+
+  function onSubmit(values: any) {
+    try {
+      console.log(values);
+      toast(
+        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+          <code className="text-white">{JSON.stringify(values, null, 2)}</code>
+        </pre>
+      );
+    } catch (error) {
+      console.error("Form submission error", error);
+      toast.error("Failed to submit the form. Please try again.");
+    }
+  }
+
+  return (
+    <><h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+    Function Level Explanations of Malware
+  </h1><Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 max-w-3xl mx-auto py-10">
+        <FormField
+          control={form.control}
+          name="fileInput"
+          render={({ field: { value, onChange, ...fieldProps } }) => (
+            <FormItem>
+              <FormLabel>Malware Sample</FormLabel>
+              <FormControl>
+                <Input
+                  {...fieldProps}
+                  placeholder="Malware Sample"
+                  type="file"
+                  accept="image/*, application/pdf"
+                  onChange={(event) => onChange(event.target.files && event.target.files[0])} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )} />
+        <FormField
+          control={form.control}
+          name="model"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Model</FormLabel>
+              <Select onValueChange={field.onChange} defaultValue={field.value}>
+                <FormControl>
+                  <SelectTrigger>
+                    <SelectValue placeholder="FLEM_FUNCTIONS_ONLY" />
+                  </SelectTrigger>
+                </FormControl>
+                <SelectContent>
+                  <SelectItem value="flem_functions_only">FUNCTIONS_ONLY</SelectItem>
+                  <SelectItem value="flem_text_section">TEXT_SECTION</SelectItem>
+                  <SelectItem value="flem_whole_exe">WHOLE_EXE</SelectItem>
+                </SelectContent>
+              </Select>
+              <FormMessage />
+            </FormItem>
+          )} />
+
+        <FormField
+          control={form.control}
+          name="algorithm"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Algorithm</FormLabel>
+              <Select onValueChange={field.onChange} defaultValue={field.value}>
+                <FormControl>
+                  <SelectTrigger>
+                    <SelectValue placeholder="LIME" />
+                  </SelectTrigger>
+                </FormControl>
+                <SelectContent>
+                  <SelectItem value="LIME">Lime</SelectItem>
+                  <SelectItem value="KERNEL SHAP">Kernel Shap</SelectItem>
+                </SelectContent>
+              </Select>
+
+              <FormMessage />
+            </FormItem>
+          )} />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form></>
+  )
+}
